1ac48b235a459219d4b1354b7d5e7029c729a109
1wildman1 <1wildman1@gmail.com>
2014-10-28 21:15:38 -0400

    Updated Queries
    Our combined work from 1-15.
    
    Only 16, 17, and the trigger left.

--- project2_queries.sql
+++ project2_queries.sql
@@ -20,7 +20,11 @@ select title, description, price, category, dateSold, dateShipped from (select *
 
 #6.	Search by keyword: Display all the items (all attributes) whose description contains the keyword “Service”
 
+select * from item where description like '%Service%';
+
 #7.	Search by category: Display all the items (all attributes) that belong to the category “Textbook”. 
+
+select * from item where category = "Textbook";
 
 #8.	View seller rating: Find the average rating of the seller who is selling item with itemID=2213
 	select AVG(rating) from sellerRating where sellerID = (Select sellerID from item where itemID = 2213) group by sellerID;
@@ -28,16 +32,23 @@ select title, description, price, category, dateSold, dateShipped from (select *
 	select count(currentbid) from bid where itemid = 2213 group by itemid;
 #10.	View most popular item: Find the item with the most number of bids.
 
+select max(item), itemid from (select count(itemid) as item, ItemID from bid group by itemid) max;
+
 #11.	Put a new item up for auction: itemID is 2215, title is " Android Cookbook”, description is “Problems and Solutions for Android Developers”, startingbid is 5, enddate is October 30th, 2014, category is “reference”, sellerID is 111.
 	insert into item values (2215, "Android Cookbook", "Problems and Solutions for Android Developers", 5, 5, 20141030,  111, "open","Reference");
 
 #12.	Ship an item: Change the status of item whose ID is 2212 to “shipped”. Also update the purchase table to indicate that the item was shipped today.
+
+update item set status = "shipped" where itemid = 2212;
+update purchase set dateShipped = sysdate() where itemid = 2212;
 
 #13.	View highest bid: Find the highest bid on the item whose ID is 2211.
 	select max(currentbid) from bid where itemid = 2211 group by itemid;
 #14.	Place a bid: Place a bid on item whose ID is 2211: the buyerID is 114, the bid is $20 and the bid is placed on October 17th,2014 at 11:25AM. 
 	insert into bid values( 114, 2211, 20141017, "11:25:00", 20);
 #15.	Rate a seller: Place a rating where buyer whose ID is 113, sellerID is 111 and the rating is 1. Use today’s date as the date for the rating. Do not specify a description.
+
+insert into sellerRating values (111, 113, 1,'', sysdate());
 
 #16.	Close an auction: Close the auction for item whose ID is 2211 by setting the highest bid as the price and today’s date as the purchase date. Note that for this query, you need to do multiple insert/update statements. 
 
